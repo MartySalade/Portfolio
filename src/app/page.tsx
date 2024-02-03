11e42fe38743e141b6f95954f36530a666af9d30
MartySalade <malleinmartin@gmail.com>
2024-02-03 15:36:37 +0100

    feat: :lipstick: glitch effect on "digital"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import "@/styles/glitch.css";
+
 import { Bento } from "@/components/Bento";
 
 export default function Home() {
@@ -6,7 +8,10 @@ export default function Home() {
     <main className="relative min-h-screen">
       <div className="absolute opacity-15 h-full w-full bg-gradient-to-r bg-opacity-10 from-black via-blue-500 to-black" />
       <div className="flex flex-col items-center p-4 sm:p-8 text-center">
-        <h1>I TURN DIGITAL CHALLENGES INTO POLISHED SOLUTIONS.</h1>
+        <h1 className="glitch layers" data-text="DIGITAL">
+          I TURN <span className="hero">DIGITAL</span> CHALLENGES INTO POLISHED
+          SOLUTIONS.
+        </h1>
         <Bento />
       </div>
     </main>
