0f887660543bc6bb9e495414382cc74bb6d7b06d
martinmallein <martin.mallein@new-immo-group.com>
2024-02-06 13:49:48 +0100

    feat: :bug: amélioration hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,40 +1,185 @@
-import { motion } from "framer-motion";
-import { ChevronsDown } from "lucide-react";
+import {
+  AnimatePresence,
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+import { ChevronsDown, MessageCircle } from "lucide-react";
 import Link from "next/link";
 
 import { LampContainer } from "@/components/ui/lamp";
+import { cn } from "@/lib/utils";
+import { useState, useRef, useEffect } from "react";
 
 export function Hero() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
+  const scale = useSpring(
+    useTransform(scrollYProgress, [0, 1], [1, 3.5]),
+    springConfig
+  );
+  const opacity = useSpring(
+    useTransform(scrollYProgress, [0, 0.8], [0.2, 0]),
+    springConfig
+  );
+  const rotateZ = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [10, 0]),
+    springConfig
+  );
+  const translateY = useSpring(
+    useTransform(scrollYProgress, [0, 1], [200, 600])
+  );
+  const translateX = useSpring(
+    useTransform(scrollYProgress, [0, 1], [100, 200])
+  );
+
   return (
-    <LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{
-          delay: 0.2,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 py-4 text-center text-4xl md:text-7xl"
-      >
-        I TURN DIGITAL CHALLENGES <br /> INTO POLISHED SOLUTIONS.
-      </motion.h1>
+    <div ref={ref}>
       <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{
-          delay: 1.5,
-          duration: 0.2,
-          ease: "easeInOut",
-        }}
-        className="absolute h-full top-full mt-24"
+        style={{ translateY, translateX }}
+        className="p-4 bg-black border-lamp rounded border w-fit shadow absolute top-16 left-16"
       >
-        <Link href="#bento">
-          <ChevronsDown className="cursor-pointer animate-pulse absolute bottom-24 sm:bottom-16" />
-        </Link>
+        <MessageCircle />
       </motion.div>
-    </LampContainer>
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 0.2,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 py-4 text-center text-4xl md:text-7xl"
+        >
+          I TURN DIGITAL CHALLENGES <br /> INTO POLISHED SOLUTIONS.
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 1.5,
+            duration: 0.2,
+            ease: "easeInOut",
+          }}
+          className="absolute h-full top-full mt-24"
+        >
+          <Link href="#bento">
+            <ChevronsDown className="cursor-pointer animate-pulse absolute bottom-24 sm:bottom-16" />
+          </Link>
+        </motion.div>
+      </LampContainer>
+      <motion.div
+        style={{ scale, opacity }}
+        className="z-[-1] grid grid-cols-12 gap-0 absolute top-0 left-0 w-screen h-screen opacity-10"
+      >
+        {[...Array(120)].map((_, index) => (
+          <div key={index} className="border border-gray-500" />
+        ))}
+      </motion.div>
+    </div>
   );
 }
+
+export const Illustration = ({ mouseEnter }: { mouseEnter: boolean }) => {
+  const stars = 1000;
+  const columns = 50;
+
+  const [glowingStars, setGlowingStars] = useState<number[]>([]);
+
+  const highlightedStars = useRef<number[]>([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      highlightedStars.current = Array.from({ length: 5 }, () =>
+        Math.floor(Math.random() * stars)
+      );
+      setGlowingStars([...highlightedStars.current]);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      className="h-48 p-1 w-full"
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        gap: `1px`,
+      }}
+    >
+      {[...Array(stars)].map((_, starIdx) => {
+        const isGlowing = glowingStars.includes(starIdx);
+        const delay = (starIdx % 10) * 0.1;
+        const staticDelay = starIdx * 0.01;
+        return (
+          <div
+            key={`matrix-col-${starIdx}}`}
+            className="relative flex items-center justify-center"
+          >
+            <Star
+              isGlowing={mouseEnter ? true : isGlowing}
+              delay={mouseEnter ? staticDelay : delay}
+            />
+            {mouseEnter && <Glow delay={staticDelay} />}
+            <AnimatePresence mode="wait">
+              {isGlowing && <Glow delay={delay} />}
+            </AnimatePresence>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const Star = ({ isGlowing, delay }: { isGlowing: boolean; delay: number }) => {
+  return (
+    <motion.div
+      key={delay}
+      initial={{
+        scale: 1,
+      }}
+      animate={{
+        scale: isGlowing ? [1, 1.2, 2.5, 2.2, 1.5] : 1,
+        background: isGlowing ? "#fff" : "#666",
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        delay: delay,
+      }}
+      className={cn("bg-[#666] h-[1px] w-[1px] rounded-full relative z-20")}
+    ></motion.div>
+  );
+};
+
+const Glow = ({ delay }: { delay: number }) => {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        delay: delay,
+      }}
+      exit={{
+        opacity: 0,
+      }}
+      className="absolute  left-1/2 -translate-x-1/2 z-10 h-[4px] w-[4px] rounded-full bg-blue-500 blur-[1px] shadow-2xl shadow-blue-400"
+    />
+  );
+};
--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -14,7 +14,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-background w-full rounded-md z-0",
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden w-full rounded-md z-0",
         className
       )}
     >
@@ -51,19 +51,13 @@ export const LampContainer = ({
           <div className="absolute  w-40 h-[100%] right-0 bg-background  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
           <div className="absolute  w-[100%] right-0 bg-background h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
-        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-background blur-2xl"></div>
+        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150  blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-lamp opacity-50 blur-3xl"></div>
-        <motion.div
-          initial={{ width: "8rem" }}
-          whileInView={{ width: "16rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
-        ></motion.div>
+
+        {/* ABOVE BAR GLOW */}
+        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-lamp opacity-30 blur-3xl" />
+
+        {/* MAIN BAR */}
         <motion.div
           initial={{ width: "15rem" }}
           whileInView={{ width: "30rem" }}
@@ -72,10 +66,10 @@ export const LampContainer = ({
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "
+          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-lamp"
         ></motion.div>
 
-        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-background "></div>
+        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem]" />
       </div>
 
       <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
